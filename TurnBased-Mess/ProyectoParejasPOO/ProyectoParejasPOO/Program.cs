5c6ab0b39e95586a700206d3970f929ba319394d
Nikooo-rp <nicolas.rojasp@upb.edu.co>
2026-03-08 14:15:06 -0500

    test

--- TurnBased-Mess/ProyectoParejasPOO/ProyectoParejasPOO/Program.cs
+++ TurnBased-Mess/ProyectoParejasPOO/ProyectoParejasPOO/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("meow");
             Console.WriteLine("Presiona cualquier tecla para comenzar...");
             Console.ReadKey();
             GameManager gameManager = new GameManager();
--- TurnBased-Mess/ProyectoParejasPOO/ProyectoParejasPOO/obj/Debug/net10.0/ProyectoParejasPOO.AssemblyInfo.cs
+++ TurnBased-Mess/ProyectoParejasPOO/ProyectoParejasPOO/obj/Debug/net10.0/ProyectoParejasPOO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoParejasPOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db140fc6f7c7241d3dccb8e5367076e946e03e92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b2d37fe18bcec4abb749de74eff4f4317ab812")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoParejasPOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoParejasPOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
